8cd027368c57b29c7e151c519e48b05126b12d93
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-01-23 11:08:49 -0600

    Added some callback functions

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-console.log('CONNECTED!');
-// First selector 
+// console.log('CONNECTED!');
+// CALLBACK FUNCTIONS
+const sentence = (name) => {
+  return `What up ${name}`;
+};
+const func1 = (string) => {
+  return `Goodbye ${string}`;
+};
 
-// let mainBtn = document.getElementById("main-btn");
-
-// The variable is the entire html button element.
-
-
-// Second selector (Common practice)
-const mainBtn = document.querySelector('#main-btn');
-console.log(mainBtn);
+const func2 = (name, cb) => {
+  return cb(name);
+};
+console.log(func2('cruel world', func1));
+console.log(func2('Jesse', sentence));

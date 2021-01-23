f03e0bdab6496a0250c3dd9e08103740c9625bbb
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-01-23 10:57:30 -0600

    Tested selectors

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 console.log('CONNECTED!');
+// First selector 
+
+// let mainBtn = document.getElementById("main-btn");
+
+// The variable is the entire html button element.
+
+
+// Second selector (Common practice)
+const mainBtn = document.querySelector('#main-btn');
+console.log(mainBtn);
